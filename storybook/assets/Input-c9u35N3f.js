506a33e7fe2a4c3f84e782cc11dfb9e5155fc7ed
hanyoung <phanyoung@naver.com>
2024-04-18 16:04:54 +0900

    step1 배포를 위한 빌드

---
+++ storybook/assets/Input-c9u35N3f.js
@@ -0,0 +1,16 @@
+import{u as s,j as a}from"./styled-components.browser.esm-BZ2fBL8i.js";const n=({isError:e=!1,...o})=>a.jsxDEV(r,{isError:e,...o},void 0,!1,{fileName:"/Users/hanyoungpark/Desktop/woowa/react-payments/src/components/common/Input.tsx",lineNumber:9,columnNumber:10},void 0),r=s.input`
+  width: ${e=>e.width};
+  padding: 10px 7px;
+
+  border: 1.2px solid ${e=>e.isError?"#ff3d3d":"#acacac"};
+  border-radius: 5px;
+  font-size: 15px;
+
+  &::placeholder {
+    color: #acacac;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;n.__docgenInfo={description:"",methods:[],displayName:"Input",props:{isError:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}}}};export{n as I};
